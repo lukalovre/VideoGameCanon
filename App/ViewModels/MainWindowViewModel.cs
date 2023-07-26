a2f135974bf0190cbc4761545ecd0f73049ae7f9
Luka Lovre <lukalovre@protonmail.com>
2023-07-26 12:15:41 +0200

    added example grid

--- App/ViewModels/MainWindowViewModel.cs
+++ App/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,20 @@
-﻿namespace App.ViewModels;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace App.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public ObservableCollection<Person> People { get; }
+
+    public MainWindowViewModel()
+    {
+        var people = new List<Person>
+        {
+            new Person("Neil", "Armstrong"),
+            new Person("Buzz", "Lightyear"),
+            new Person("James", "Kirk")
+        };
+        People = new ObservableCollection<Person>(people);
+    }
 }
