15e0074a788a0b0a830487532323d30b1c239c66
Luka Lovre <lukalovre@protonmail.com>
2023-07-27 14:17:10 +0200

    updated input and putput models

--- App/Models/InputGameData.cs
+++ App/Models/InputGameData.cs
@@ -1,4 +1,4 @@
-public class Game
+public class InputGameData
 {
     public int ID { get; set; }
     public string Title { get; set; }
---
+++ App/Models/OutputGameData.cs
@@ -0,0 +1,11 @@
+public class OutputGameData
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public string Url { get; set; }
+    public string Summary { get; set; }
+    public int Year { get; set; }
+    public string DeveloperName { get; set; }
+    public long DeveloperID { get; set; }
+    public long GameID { get; set; }
+}
